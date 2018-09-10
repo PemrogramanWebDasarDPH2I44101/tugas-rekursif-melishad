72ae4a6cc6cf1b39a8f247080ad35343843ba44f
melishad <42725372+melishad@users.noreply.github.com>
2018-09-10 19:55:52 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,42 @@
-<?php
+<?php 
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+$z = 0;
+function rekursif2 ($x, $y) {
+	global $z;
+	if ($x > $z - $y) {
+		echo "*";
+		rekursif2($x -1, $y);
+	}
 }
-rekursif(5);
-?>
+function rekursif1($y){
+	global $z;
+	rekursif2($z, $y);
+	echo "<br>";
+	if (--$y > $z) {
+		rekursif1($y);
+			
+		}
+	}
+
+rekursif1(5);
+
+	echo "/*====================================================================================*/<br>";
+
+
+function rekursif($b,$c,$a) {
+	if ($b <= $a) {
+		if ($c <= $b) {
+			echo $c;
+			$c++;
+			rekursif($b, $c, $a);
+		}
+		else {
+			echo "<br>";
+			$b++;
+			rekursif($b,1,$a);
+		}
+	}
+}
+$a = 5;
+	rekursif(1,1,$a);
+ ?>
